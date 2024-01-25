639f787b25eb82308f3bfee9361c678c589cc9db
yandongxu <hello@yandongxu.com>
2024-01-26 00:24:51 +0800

    feat: ESLint support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" }, -- TypeScript support
+    { import = "lazyvim.plugins.extras.linting.eslint" }, -- ESLint support
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
