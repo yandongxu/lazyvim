6e04448c222ec71fbb433a0e0ff342ec3a034aeb
yandongxu <hello@yandongxu.com>
2024-01-26 01:57:36 +0800

    feat: ESLint-plugin-prettier support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,28 @@ return {
   },
 
   -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    -- NOTE 使用 `eslint-plugin-pretter` 使用 `eslint` 修复错误，使用 `prettier` 格式化代码
+    opts = {
+      servers = {
+        eslint = {},
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+
+  --
   -- TODO: 使用 `Emmet LS` 替代 `Emmet language server`, 解决 JSX className 问题
   -- # Emmet language server
   -- (Emmet language server)[https://github.com/olrtg/emmet-language-server?tab=readme-ov-file]
