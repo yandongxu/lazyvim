6321e4c43f828773179f77ed72d231c138fabb7b
yandongxu <hello@yandongxu.com>
2024-01-26 01:49:06 +0800

    feat: update LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,4 +13,15 @@ return {
       },
     },
   },
+
+  -- LSP
+  -- TODO: 使用 `Emmet LS` 替代 `Emmet language server`, 解决 JSX className 问题
+  -- # Emmet language server
+  -- (Emmet language server)[https://github.com/olrtg/emmet-language-server?tab=readme-ov-file]
+  -- # Emmet LS
+  -- (Emmet LS)[https://github.com/aca/emmet-ls]
+  -- (Emmet config)[https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267]
+  -- {
+  --   "neovim/nvim-lspconfig",
+  -- },
 }
