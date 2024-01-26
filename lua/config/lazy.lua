fc76aa6c4204e490716cb977a6956694e8884139
yandongxu <yandx@greatld.com>
2024-01-26 09:58:48 +0800

    feat: Project support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" }, -- TypeScript support
     { import = "lazyvim.plugins.extras.linting.eslint" }, -- ESLint support
     { import = "lazyvim.plugins.extras.coding.codeium" }, -- Codeium support
+    { import = "lazyvim.plugins.extras.util.project" }, -- Project support
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
