abad4e90924233b809fd746c169895879bedbe8a
yandongxu <yandx@greatld.com>
2024-01-26 13:59:02 +0800

    feat update Animation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,18 +1,14 @@
 return {
-  ----------------------
-  -- Lazy default config
-  ----------------------
-
   -- 代码块对称高亮
-  -- {
-  --   "echasnovski/mini.indentscope",
-  --   opts = {
-  --     draw = {
-  --       -- delay = 10,
-  --       animation = function(s, n)
-  --         return 5
-  --       end,
-  --     },
-  --   },
-  -- },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        -- delay = 10,
+        animation = function()
+          return 2
+        end,
+      },
+    },
+  },
 }
