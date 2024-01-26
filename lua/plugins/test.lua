289e9c04a8182f0c0f6a5bc5627193fbd3829d59
yandongxu <yandx@greatld.com>
2024-01-26 14:25:15 +0800

    feat: update Neotest config

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -7,10 +7,22 @@ return {
       "nvim-treesitter/nvim-treesitter",
 
       "marilari88/neotest-vitest",
+      "thenbe/neotest-playwright",
     },
     opts = {
+      status = { virtual_text = true },
+      output = { open_on_run = true },
       adapters = {
-        "neotest-vitest",
+        {
+          ["neotest-vitest"] = {},
+
+          ["neotest-playwright"] = {
+            option = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          },
+        },
       },
     },
   },
