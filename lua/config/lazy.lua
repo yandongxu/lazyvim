fcc5ccf2169284c4bf7ec693dd7850bdaa1ce28e
yandongxu <hello@yandongxu.com>
2024-01-28 02:50:51 +0800

    feat: DAP support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" }, -- Project support
     { import = "lazyvim.plugins.extras.test.core" }, -- Testing support
     { import = "lazyvim.plugins.extras.dap.core" }, -- DAP support
+    { import = "lazyvim.plugins.extras.dap.nlua" }, -- DAP Lua support
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
